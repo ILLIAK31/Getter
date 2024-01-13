4584db659bf93d2f86065f4114ceddfa1bb79fec
Illia <karma@MYPC>
2024-01-13 15:37:32 +0100

    Add project files.

---
+++ Getter/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Getter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_end = new System.Windows.Forms.Button();
+            this.button_search = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Device";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 68);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Search";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(43, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(237, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(43, 456);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(237, 24);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(43, 199);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(237, 64);
+            this.button_start.TabIndex = 4;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_end
+            // 
+            this.button_end.Location = new System.Drawing.Point(43, 285);
+            this.button_end.Name = "button_end";
+            this.button_end.Size = new System.Drawing.Size(237, 64);
+            this.button_end.TabIndex = 5;
+            this.button_end.Text = "End";
+            this.button_end.UseVisualStyleBackColor = true;
+            this.button_end.Click += new System.EventHandler(this.button_end_Click);
+            // 
+            // button_search
+            // 
+            this.button_search.Location = new System.Drawing.Point(43, 530);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(237, 64);
+            this.button_search.TabIndex = 6;
+            this.button_search.Text = "Search";
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.button_search_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(328, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(685, 608);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 656);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_search);
+            this.Controls.Add(this.button_end);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_end;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Getter/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
+using System.Management;
+
+namespace Getter
+{
+    public partial class Form1 : Form
+    {
+        static List<string> list = new List<string>
+        {
+                "Architecture",
+                "AssetTag",
+                "Availability",
+                "Caption",
+                "Characteristics",
+                "ConfigManagerErrorCode",
+                "ConfigManagerUserConfig",
+                "CpuStatus",
+                "CreationClassName",
+                "CurrentClockSpeed",
+                "CurrentVoltage",
+                "DataWidth",
+                "Description",
+                "DeviceID",
+                "ErrorCleared",
+                "ErrorDescription",
+                "ExtClock",
+                "Family",
+                "InstallDate",
+                "L2CacheSize",
+                "L2CacheSpeed",
+                "L3CacheSize",
+                "L3CacheSpeed",
+                "LastErrorCode",
+                "Level",
+                "LoadPercentage",
+                "Manufacturer",
+                "MaxClockSpeed",
+                "Name",
+                "NumberOfCores",
+                "NumberOfEnabledCore",
+                "NumberOfLogicalProcessors",
+                "OtherFamilyDescription",
+                "PartNumber",
+                "PNPDeviceID",
+                "PowerManagementCapabilities",
+                "PowerManagementSupported",
+                "ProcessorId",
+                "ProcessorType",
+                "Revision",
+                "Role",
+                "SecondLevelAddressTranslationExtensions",
+                "SerialNumber",
+                "SocketDesignation",
+                "Status",
+                "StatusInfo",
+                "Stepping",
+                "SystemCreationClassName",
+                "SystemName",
+                "ThreadCount",
+                "UniqueId",
+                "UpgradeMethod",
+                "Version",
+                "VirtualizationFirmwareEnabled"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("CPU");
+        }
+        void CPU_INFO()
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns.Add("CategoryColumn", "AddressWidth");
+            ManagementObjectSearcher obj = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            foreach (ManagementObject obj2 in obj.Get())
+            {
+                dataGridView1.Columns.Add("CategoryColumn", Convert.ToString(obj2["AddressWidth"]));
+            }
+            foreach (ManagementObject obj2 in obj.Get())
+            {
+                foreach (string item in list)
+                {
+                    dataGridView1.Rows.Add(item, Convert.ToString(obj2[item]));
+                }
+            }
+            try
+            {
+                OpenHardwareMonitor.Hardware.Computer computer = new OpenHardwareMonitor.Hardware.Computer();
+                computer.CPUEnabled = true;
+                computer.Open();
+                foreach (var hardware in computer.Hardware)
+                {
+                    if (hardware.HardwareType == HardwareType.CPU)
+                    {
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Temperature)
+                            {
+                                dataGridView1.Rows.Add("CPU Temperature", $"{sensor.Value} °C");
+                            }
+                        }
+                    }
+                }
+                computer.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            string selectedProcessor = comboBox1.SelectedItem?.ToString();
+            if (!string.IsNullOrEmpty(selectedProcessor) && selectedProcessor.Equals("CPU", StringComparison.OrdinalIgnoreCase))
+            {
+                comboBox2.Items.Add("AddressWidth");
+                foreach (string item in list)
+                {
+                    comboBox2.Items.Add(item);
+                }
+                CPU_INFO();
+            }
+        }
+
+        private void button_end_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+        }
+
+        private void button_search_Click(object sender, EventArgs e)
+        {
+            string selectedProcessor = comboBox2.SelectedItem?.ToString();
+            if (!string.IsNullOrEmpty(selectedProcessor))
+            {
+                ManagementObjectSearcher obj = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+                foreach (ManagementObject obj2 in obj.Get())
+                {
+                    MessageBox.Show(selectedProcessor + " : " + Convert.ToString(obj2[selectedProcessor]));
+                }
+            }
+        }
+    }
+}
